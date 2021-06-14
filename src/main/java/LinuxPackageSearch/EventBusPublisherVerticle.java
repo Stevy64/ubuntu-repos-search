26b415a522edab04b277068df859c41f068f5ce7
sba <s.boubala@novatice.com>
2021-06-14 16:00:14 +0200

    Reformat code

--- src/main/java/LinuxPackageSearch/EventBusPublisherVerticle.java
+++ src/main/java/LinuxPackageSearch/EventBusPublisherVerticle.java
@@ -7,31 +7,32 @@ package LinuxPackageSearch;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 
 /**
  *
  * @author steevy
  */
 public class EventBusPublisherVerticle extends AbstractVerticle {
-    
-    String pkgName = "firefox";
-    
+
+	private static String UBUNTU_REPOS = "firefox";
+	private static String[] UBUNTU_CODE_NAME = { "focal", "bionic" };
+
 	@Override
-	public void start (Future<Void> startFuture) throws Exception {
-        vertx.eventBus().request("linux", pkgName, ar -> {
-        	  if (ar.succeeded()) {
-        		  System.out.println("pkgName sent = " + pkgName);
-        		  System.out.println("Package received back = " + ar.result().body());
-        		    }
-        	  else {
-        		  System.out.println("Failure -- " + ar.cause());
-        	  }
-        	});
+	public void start(Future<Void> startFuture) throws Exception {
+		vertx.eventBus().request("kit.applications.chocolatey.package.search",
+				new JsonObject().put("Package", UBUNTU_REPOS).put("UbuntuCodeName", UBUNTU_CODE_NAME[0]), ar -> {
+					if (ar.succeeded()) {
+						// System.out.println("pkgName sent = " + );
+						System.out.println("Package received back = " + ar.result().body());
+					} else {
+						System.out.println("Failure -- " + ar.cause());
+					}
+				});
 	}
-	
+
 	@Override
 	public void stop() throws Exception {
 		System.out.println("Verticle stopped on chanel : kit.applications.package.name.search");
 	}
 }
-
